47150c3ed03dd868765812fc2f21d9eb17b17f8f
Jan Filipp <filipp.jan@protonmail.com>
2020-12-12 14:11:54 +0100

    document SingleValueW

--- include/index/SingleValueW.h
+++ include/index/SingleValueW.h
@@ -5,6 +5,19 @@
 
 namespace epic::index {
 
+/**
+ * Implementing the ItfPowerIndex interface to calculate the single value \f$|W|\f$ where \f$W\f$ is the set of winning coalitions. The calculation is based on the <i>raw Banzhaf</i> power index.
+ *
+ * <table>
+ * 		<caption>Behaviour on index specific CLI-flags</caption>
+ * 		<tr><th>Flag<th>Behaviour
+ * 		<tr><td>`--verbose / -v`		<td>no extra output
+ * 		<tr><td>`--filter-null / -f`	<td>no special behaviour
+ * </table>
+ *
+ * <b>Implementation note</b>:
+ * If the mGame contains player of weight zero, they get excluded from the calculation. At the end the result gets properly scaled as if the players of weight zero were part of the calculation.
+ */
 class SingleValueW : public RawBanzhaf {
 public:
 	SingleValueW(Game& g, ItfUpperBoundApproximation* approx, IntRepresentation int_representation = DEFAULT);
