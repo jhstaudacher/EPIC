fdf4a626a9f04b81cb271785535d0d58287e3a57
Jan Filipp <filipp.jan@protonmail.com>
2021-12-31 12:32:10 +0100

    cleanup OwenExtendedPGI header

--- include/index/OwenExtendedPGI.h
+++ include/index/OwenExtendedPGI.h
@@ -5,13 +5,7 @@
 
 namespace epic::index {
 
-/**
- * Add here a description of the new power index for the Doxygen documentation generator.
- */
 class OwenExtendedPGI : public PowerIndexWithPrecoalitions {
-	/*
- * Implement the ItfPowerIndex interface:
- */
 public:
 	OwenExtendedPGI();
 
@@ -20,11 +14,8 @@ public:
 	longUInt getMemoryRequirement(Game* g) override;
 	bigInt getMaxValueRequirement(ItfUpperBoundApproximation* approx) override;
 	lint::Operation getOperationRequirement() override;
-
-//private:
-//	bigInt mBigTmp;
 };
 
 } // namespace epic::index
 
-#endif 
+#endif // EPIC_INDEX_OWENEXTENDEDPGI_H_
